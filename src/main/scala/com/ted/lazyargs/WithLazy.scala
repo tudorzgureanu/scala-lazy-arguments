62b1bd4fd1feb3918ed7c096b5c13ecd8aa834cc
Tudor Zgureanu <tzgureanu@gmail.com>
2017-05-17 23:48:26 +0100

    Initial implementation of by-need args.
    Allow `@Lazy` to be used only for simple arguments defined in by-value style.
    Raise compile error for using `@Lazy` on by-name arguments or varargs.

--- src/main/scala/com/ted/lazyargs/WithLazy.scala
+++ src/main/scala/com/ted/lazyargs/WithLazy.scala
@@ -2,7 +2,7 @@ package com.ted.lazyargs
 
 import scala.annotation.StaticAnnotation
 import scala.collection.immutable.Seq
-import scala.meta.Type.Arg.ByName
+import scala.meta.Type.Arg
 import scala.meta._
 import scala.meta.contrib._
 
@@ -11,20 +11,22 @@ class WithLazy extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     defn match {
       case defn: Defn.Def =>
-//        println(defn.syntax)
         val updatedParamss = defn.paramss.map { params =>
           params.map {
             case lazyParam@Term.Param(_, _, Some(tpe: Type.Name), _) if WithLazy.isLazy(lazyParam) =>
-              lazyParam.copy(decltpe = Some(ByName(tpe)))
+              lazyParam.copy(decltpe = Some(Arg.ByName(tpe)))
             case other => other
           }
         }
         val lazyAnnotatedParams = WithLazy.collectLazyArgs(defn.paramss)
-//        println(lazyAnnotatedParams)
         val localLazyValDefinitions: Seq[Defn.Val] = lazyAnnotatedParams.map {
           case param@Term.Param(_, name, Some(tpe: Type.Name), _) =>
             val lazyTerm = Pat.Var.Term(Term.Name(WithLazy.addLazySuffix(name.value)))
             q"lazy val $lazyTerm: $tpe = ${param.name.asTerm}"
+          case param@Term.Param(_, _, Some(_: Arg.Repeated), _) =>
+            abort(param.pos, "@Lazy varargs not supported yet.")
+          case other =>
+            abort(other.pos, "No @Lazy parameter allowed here.")
         }
         val innerDefn = defn.copy(name = Term.Name(WithLazy.addInnerSuffix(defn.name.value)), paramss = updatedParamss)
         val replaceWith = lazyAnnotatedParams.map(param => param.name.value -> Term.Name(WithLazy.addLazySuffix(param.name.value))).toMap
@@ -38,8 +40,6 @@ class WithLazy extends StaticAnnotation {
               }
            """
         val updatedDefn = defn.copy(paramss = updatedParamss, body = updatedBody)
-//        println(s"Generated definition for ${defn.name}:")
-//        println(updatedDefn.syntax)
         updatedDefn
       case other => abort(other.pos, "@WithLazy must annotate a method.")
     }
