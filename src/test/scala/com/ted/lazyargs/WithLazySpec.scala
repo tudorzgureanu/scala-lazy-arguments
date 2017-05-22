3cca76e5f7ec75823935f0bff4f72a66be05740f
Tudor Zgureanu <tzgureanu@gmail.com>
2017-05-22 13:30:55 +0100

    Simple test cases for @WithLazy and @Lazy.

---
+++ src/test/scala/com/ted/lazyargs/WithLazySpec.scala
@@ -0,0 +1,37 @@
+package com.ted.lazyargs
+
+import org.scalatest.{Matchers, WordSpecLike}
+
+class WithLazySpec extends WordSpecLike with Matchers {
+
+  "WithLazy" when {
+    "annotates a simple method" should {
+      @WithLazy
+      def lazyFoo(cond: Boolean)(@Lazy bar: String) = if (cond) bar + bar else ""
+
+      "evaluate the @Lazy block only once on `cond` being true" in {
+        var nastyVar = 0
+
+        val result = lazyFoo(cond = true) {
+          nastyVar += 1
+          "bar "
+        }
+
+        result shouldBe "bar bar "
+        nastyVar shouldBe 1
+      }
+
+      "not evaluate the @Lazy block at all on `cond` being false" in {
+        var nastyVar = 0
+
+        val result = lazyFoo(cond = false) {
+          nastyVar += 1
+          "bar "
+        }
+
+        result shouldBe ""
+        nastyVar shouldBe 0
+      }
+    }
+  }
+}
